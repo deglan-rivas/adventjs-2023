f4cbb8ac549fbed656881c35e1a35ec7ba89f4b6
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-02-29 15:36:08 -0500

    [REFACTOR] ex 1

--- src/01.js
+++ src/01.js
@@ -6,17 +6,21 @@ export const findFirstRepeated = (numbers) => {
   if (unrepeatedNumbers.length === numbers.length) return -1
   // console.log(unrepeatedNumbers)
 
-  let firstRepeated = -1
-  let hasFound = false
-  numbers.forEach((number, index) => {
-    if (unrepeatedNumbers[index] !== number && !hasFound) {
-      firstRepeated = number
-      hasFound = true
-    }
-  })
+  // let firstRepeated = -1
+  // let hasFound = false
+  // numbers.forEach((number, index) => {
+  //   if (unrepeatedNumbers[index] !== number && !hasFound) {
+  //     firstRepeated = number
+  //     hasFound = true
+  //   }
+  // })
 
+  // return firstRepeated
+
+  // REFACTOR 1
+  const firstRepeated = numbers.find((number, index) => unrepeatedNumbers[index] !== number)
   return firstRepeated
 }
 
-const a = findFirstRepeated([1, 2, 3, 3, 5])
-console.log(a)
+// const a = findFirstRepeated([1, 2, 3, 3, 5])
+// console.log(a)
