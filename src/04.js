e0d6e7be1eb58c9708b962d93779630853523063
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-16 11:21:34 -0500

    [UPDATE] ex 04

--- src/04.js
+++ src/04.js
@@ -1,3 +1,24 @@
 export function decode (message) {
   if (typeof message !== 'string') throw new Error('message must be a string')
+
+  const messageAux = message.split('')
+  console.log(messageAux)
+
+  const firstBracket = messageAux.findIndex(letter => letter === '(')
+  const lastBracket = messageAux.findLastIndex(letter => letter === ')')
+
+  console.log(firstBracket, lastBracket)
+
+  const contentBracket = messageAux.slice(firstBracket + 1, lastBracket)
+  console.log(contentBracket)
+
+  const reverseContent = contentBracket.reduce((acc, letter, index, array) => acc + array[array.length - index - 1], '')
+  console.log(reverseContent)
+
+  const finalContentStart = message.split('(', 2)[0]
+  console.log(finalContentStart)
+  const finalContentEnd = message.split('(', 2)[1].split(')', 2)[1]
+  console.log(finalContentStart, finalContentEnd)
+
+  return finalContentStart + reverseContent + finalContentEnd
 }
--- test/04.test.js
+++ test/04.test.js
@@ -5,10 +5,15 @@ describe('testing example 04', () => {
   it('should be a function', () => {
     expect(typeof decode).toBe('function')
   })
+
   it('should throw error if message is not string', () => {
     expect(() => decode(1)).toThrow()
     expect(() => decode([])).toThrow()
     expect(() => decode({})).toThrow()
     expect(() => decode(true)).toThrow()
+  })
+
+  it('should return decoded message', () => {
+    expect(decode('hola (odnum) titis')).toBe('hola mundo titis')
   })
 })
