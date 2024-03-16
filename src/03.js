447c307ecb604ee14343eebbdcaaf4cc757156ad
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-15 23:46:42 -0500

    [ADD] set ex 04

--- src/03.js
+++ src/03.js
@@ -1,3 +1,22 @@
 export const findNaughtyStep = (original, modified) => {
-  ''
+  if (typeof original !== 'string') throw new Error('original must be a string')
+  if (typeof modified !== 'string') throw new Error('modified must be a string')
+
+  if (original === modified) return ''
+  // convert 'abcde' into ['a', 'b', 'c', 'd', 'e']
+  // original = [...new Set(original)]
+  original = original.split('')
+  modified = modified.split('')
+  console.log(modified)
+
+  // el error estaba en usar letter === original
+  // en el ternario siempre agregar el ? '' : '' ambos casos
+  // no dejar el : '' porque sino funciona incorrectamente tmr xD
+  // siempre darle valor inicial al acumulador o tomará el primer valor del string y eso es peligroso para este ejemplo xd
+  const naughtyLetters = original.reduce((acc, letter, index) => letter !== modified[index] ? letter !== modified[index + 1] ? acc += letter : acc += modified[index] : acc, '')
+  console.log(naughtyLetters)
+  return naughtyLetters ? naughtyLetters[0] : modified[modified.length - 1]
 }
+
+// findNaughtyStep('hello', 'hola')
+findNaughtyStep('abcd', 'abcde')
--- test/03.test.js
+++ test/03.test.js
@@ -5,4 +5,21 @@ describe('testing example 03', () => {
   it('should be a function', () => {
     expect(typeof findNaughtyStep).toBe('function')
   })
+
+  it('should throw error if both strings are not provided', () => {
+    expect(() => findNaughtyStep([], 2)).toThrow()
+    expect(() => findNaughtyStep('ga', true)).toThrow()
+    expect(() => findNaughtyStep(true, 'ga')).toThrow()
+  })
+
+  it('should return \'\' if there is no naughty', () => {
+    expect(findNaughtyStep('hello', 'hello')).toBe('')
+  })
+
+  it('should return first naughty letter', () => {
+    expect(findNaughtyStep('hello', 'hola')).toBe('e')
+    expect(findNaughtyStep('stepfor', 'stepor')).toBe('f')
+    expect(findNaughtyStep('abcd', 'abcde')).toBe('e')
+    expect(findNaughtyStep('xxxx', 'xxoxx')).toBe('o')
+  })
 })
