d608905b3a7ca18e0ccece87fb930c685f950152
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-19 22:40:03 -0500

    [UPDATE] pending fix for bracket examples like (()())

--- src/04.js
+++ src/04.js
@@ -22,3 +22,38 @@ export function decode (message) {
 
   return finalContentStart + reverseContent + finalContentEnd
 }
+
+export function decodeRefactor (message) {
+  if (typeof message !== 'string') throw new Error('message must be a string')
+
+  if (!message.includes('(') || !message.includes(')')) return message
+
+  const startIndex = message.indexOf('(')
+  const lastIndex = message.lastIndexOf(')')
+  const startContent = message.slice(0, startIndex)
+  const bracketContent = message.slice(startIndex + 1, lastIndex)
+  const endContent = message.slice(lastIndex + 1)
+
+  console.log(startContent, (bracketContent), endContent)
+  return startContent + decodeRefactor(reverse(bracketContent)) + endContent
+}
+
+export function reverse (string) {
+  return string.split('').reduce((acc, letter, index, array) => {
+    const mirrorLetter = array[array.length - index - 1]
+    if (mirrorLetter === ')') return acc + '('
+    if (mirrorLetter === '(') return acc + ')'
+    return acc + mirrorLetter
+  }, '')
+}
+
+const a = decodeRefactor('hola (odnum) titis')
+// const b = decodeRefactor('hola (o(und)m) titis')
+// const c = decodeRefactor('hola (o(u(dn))m) titis')
+
+// para darle submit al adventjs hay que usar function en lugar de arrow function externo, no usar import export ni throw error, todo el código dentro de una sola función
+// tmr fallaron 2 casos fuera de los test de prueba que da adventjs -> el tdd se hace justamente para evitar hacer cambios drásticos más adelante: hacer cambios en la semana o sprint5 es distinto a hacerlo en la semana o sprtin1
+// por eso llenar de todos los test rojos o casos más disparatados y recién sobre todos esos casos comenzar a extraer la lógica
+// falló Test: decode('(olleh) (dlrow)!') y Test: decode('((nta)(sa))') ->
+// 1 6 vs 4 9  -> 1 4 ; 6 9
+// 1 2 5 vs 4 8 9 -> 1 9 ; 2 4 ; 5 8
--- test/04.test.js
+++ test/04.test.js
@@ -1,19 +1,21 @@
 import { describe, expect, it } from 'vitest'
-import { decode } from '../src/04'
+import { decodeRefactor } from '../src/04'
 
 describe('testing example 04', () => {
   it('should be a function', () => {
-    expect(typeof decode).toBe('function')
+    expect(typeof decodeRefactor).toBe('function')
   })
 
   it('should throw error if message is not string', () => {
-    expect(() => decode(1)).toThrow()
-    expect(() => decode([])).toThrow()
-    expect(() => decode({})).toThrow()
-    expect(() => decode(true)).toThrow()
+    expect(() => decodeRefactor(1)).toThrow()
+    expect(() => decodeRefactor([])).toThrow()
+    expect(() => decodeRefactor({})).toThrow()
+    expect(() => decodeRefactor(true)).toThrow()
   })
 
-  it('should return decoded message', () => {
-    expect(decode('hola (odnum) titis')).toBe('hola mundo titis')
+  it('should return decodeRefactord message', () => {
+    expect(decodeRefactor('hola (odnum) titis')).toBe('hola mundo titis')
+    expect(decodeRefactor('hola (o(und)m) titis')).toBe('hola mundo titis')
+    expect(decodeRefactor('hola (o(u(dn))m) titis')).toBe('hola mundo titis')
   })
 })
